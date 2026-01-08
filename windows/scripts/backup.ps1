ef047f1f58cd48ae22aa39536fbe4ba5f618b2b5
madkarmaa <madkarma99@gmail.com>
2026-01-08 15:35:44 +0100

    feat: backup PowerToys settings

--- windows/scripts/backup.ps1
+++ windows/scripts/backup.ps1
@@ -10,3 +10,10 @@ function New-Subdir {
 Copy-Item "$env:USERPROFILE\.config\yasb\*" -Destination (New-Subdir "yasb") -Recurse -Force -Exclude "yasb.log"
 Copy-Item "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Destination (New-Subdir "powershell") -Force
 Copy-Item "$env:USERPROFILE\.config\fastfetch\*" -Destination (New-Subdir "fastfetch") -Recurse -Force
+
+$latestPowerToysBackup = Get-ChildItem -Path "$env:USERPROFILE\Documents\PowerToys\Backup" -Filter "*.ptb" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+if ($latestPowerToysBackup) {
+    Copy-Item $latestPowerToysBackup.FullName -Destination (New-Subdir "powertoys") -Force
+} else {
+    Write-Host "No PowerToys backup file found." -ForegroundColor Yellow
+}
